dfa49d3cdf6be55a03e7d46ddd6caed873df67cd
david3464 <51375293+david3464@users.noreply.github.com>
2019-08-21 18:42:40 -0400

    1

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ else { console.log('Error in DB connection : ' + err) }
 //Use Routes
 app.use('api/items', items);
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on Port ${port}...`));
   
